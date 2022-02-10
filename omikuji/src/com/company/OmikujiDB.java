d4ce5e8741d21da77fcb7882ab5bcba306c69223
hiyony <hing9u.k@gmail.com>
2022-02-10 17:35:45 +0900

    220210 updated(+DB)

--- omikuji/src/com/company/OmikujiDB.java
+++ omikuji/src/com/company/OmikujiDB.java
@@ -1,20 +1,74 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OmikujiDB {
-    public static void main(String [] args){
-        Connection con = null;
+    public void birthday() throws IOException {
+        String birthday;
+        BufferedReader br2;
+        SimpleDateFormat dateFP;
+        DateTimeFormatter datetimeFP;
 
+        LocalDate today = LocalDate.now();
+        datetimeFP = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String todayString = today.format(datetimeFP);
+
+        br2 = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            System.out.println("お誕生日はいつですか？(yyyyMMdd 形式)　");
+            birthday = br2.readLine();
+            if (birthday.length() != 8) {
+                System.out.println("yyyyMMddの8個の数字でお願いします。");
+                continue;
+            }
+            try {
+                dateFP = new SimpleDateFormat("yyyyMMdd");
+                dateFP.setLenient(false);
+                dateFP.parse(birthday);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void omikuji_db() throws IOException {
+        birthday();
+
+        //Connection: 데이터베이스와 연결하는 객체　データベースと連結するオブジェクト
+        //Statement: 정적 Query를 처리할 때 유리하도록 만드는 객체 >> SELECT query에 이용
+        //静的クエリを処理する場合使うオブジェクト >> SELECT Queryで使う
+        //PreparedStatement: 동적 Query를 처리할 때 유리하도록 만드는 객체  >> INSERT query에 이용
+        //動的クエリを処理する場合使うオブジェクト >> INSERT Queryで使う
+        Connection conn = null;
+        Statement stmt = null;
+        PreparedStatement pstmt = null;
+        String unsei_sql;
+        String result_sql = null;
         String server = "localhost"; //MySQL 서버주소 サーバーのアドレス
         String database = ""; //MySQL DB명　サーバーの名
         String user_name = "root"; //MySQL 서버ID　サーバーのID
         String password = "1005"; //MySQL 서버PW　サーバーのPW
 
+        Unsei unsei = null;
+        List<Unsei> omikuji = new ArrayList<>();
+        String path = "omikuji/omkj.csv";
+        String line;
+        String [] values;
+        BufferedReader br = null;
+
+
         //1. 드라이버 로딩 ドライバーのロディング
         try{
+            //MySQL의 JDBC Driver Class를 로딩　MySQLのJDBCドライバーのクラスをロディング
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e){
             System.err.println("<JDBC エラー> ドライバーロディングのエラー: " + e.getMessage());
@@ -23,20 +77,83 @@ public class OmikujiDB {
 
         //2. 접속 接続
         try {
-            con = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database, user_name, password);
-            System.out.println("正常に接続されました。");
+            //DriverManager는 JDBC드라이버를 통해 Connection을 만드는 역할
+            //DriverManagerはJDBCドライバーを通じてConnectionを作る役割
+            conn = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database, user_name, password);
+            unsei_sql = "INSERT INTO omikujii(運勢コード, 願い事, 商い, 学問) VALUES(?, ?, ?, ?)"; //쿼리문　クエリ文
+            pstmt = conn.prepareStatement(unsei_sql); //쿼리를 실행할 객체를 생성(객체 생성시에 쿼리를 준다)
+                                                      //クエリを実装するオブジェクトを作る(オブジェクトを作るとクエリが与えられる）
+            br = new BufferedReader(new FileReader(path));
+
+            br.readLine();
+            while((line = br.readLine()) != null){
+                values = line.split(",");
+                unsei = selectUnsei(values[0]);
+                unsei.setUnsei();
+                unsei.setNegaigoto(values[1]);
+                unsei.setAkinai(values[2]);
+                unsei.setGakumon(values[3]);
+                omikuji.add(unsei);
+            }
+
+            //DB에 ArrayList 내용 넣기 DBにArrayListの内容を入れる
+            int size = omikuji.size();
+            for (int i = 0; i < size; i++){
+                Unsei fortune = omikuji.get(i);
+                pstmt.setString(1, fortune.getUnsei());
+                pstmt.setString(2, fortune.getNegaigoto());
+                pstmt.setString(3, fortune.getAkinai());
+                pstmt.setString(4, fortune.getGakumon());
+
+                //특정 내용을 데이터베이스에 적용시켜야 할 경우 executeUpdate() 메서드를 사용
+                //特定内容をデータベースに適用される場合、executeUpdate()　メソッドを使う
+                pstmt.executeUpdate();
+            }
         } catch (SQLException e){
-            System.out.println("con エラー: "+ e.getMessage());
             e.printStackTrace();
+        } finally {
+            //3. 해제 解除
+            try{
+                if(br!=null){br.close();}
+                if(pstmt != null){pstmt.close();}
+                if(conn != null){conn.close();}
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
-        //3. 해제 解除
-        try{
-            if(con != null){
-                con.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+    }
+
+    public static Unsei selectUnsei(String unseistr) {
+        Unsei unsei = null;
+        switch (unseistr) {
+            case "大吉":
+                unsei = new Daikichi();
+                break;
+            case "中吉":
+                unsei = new Cyuukichi();
+                break;
+            case "小吉":
+                unsei = new Syoukichi();
+                break;
+            case "吉":
+                unsei = new Kichi();
+                break;
+            case "末吉":
+                unsei = new Sueyosi();
+                break;
+            case "凶":
+                unsei = new Kyou();
+                break;
+            default:
+                break;
         }
+        return unsei;
+    }
+
+    public static void main(String [] args) throws IOException {
+        OmikujiDB omikujiDB = new OmikujiDB();
+        omikujiDB.omikuji_db();
     }
 }
--- omikuji/src/com/company/Omikujiprogram.java
+++ omikuji/src/com/company/Omikujiprogram.java
@@ -1,6 +1,11 @@
 package com.company;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -8,11 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
 public class Omikujiprogram {
 
-    @SuppressWarnings("null")
-    public static void omikujii() throws Exception {
+    public void omikujii() throws Exception {
 
         //1. 생일 입력 받기 誕生日入力を求める
         //Birthday 変数を宣言します。
@@ -43,7 +46,8 @@ public class Omikujiprogram {
                 dateFormatParser.setLenient(false);
                 dateFormatParser.parse(birthday);
                 break;
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 System.out.println("入力された誕生日の日付形式が正しくありません。");
                 System.out.println("YYYYMMDDの形式でお願いします。");
             }
@@ -63,21 +67,19 @@ public class Omikujiprogram {
         //全ての結果が入れるresult.csvを読み込みます。
         List<Unsei> omikuji = new ArrayList<>(); //부모클래스 Unsei로 생성한 객체 어레이리스트
         //Unsei unsei = null; //추상 클래스는 단독으로 객체 생성이 불가능함
-        Unsei unsei = new Unsei() {
-            @Override
-            public void setUnsei() {
-
-            }
-        };
+        //Unsei unsei = new Unsei() {
+        //@Override
+        //public void setUnsei() {
+        //
+        //  }
+        //};
+        Unsei unsei = null;
         File result = new File("omikuji/result.csv");
 
-
-        //3. CSV파일에 결과 입력(이미 저장된 결과가 있을 경우) CSVファイルに結果をセットする(UNSEIにもう結果がある場合)
+        //4. CSV파일에 결과 입력(이미 저장된 결과가 있을 경우) CSVファイルに結果をセットする(UNSEIにもう結果がある場合)
         try {
             FileReader reader = new FileReader(result);
             BufferedReader br3 = new BufferedReader(reader);
-            FileWriter fw = new FileWriter(result, true);
-            String ls = System.lineSeparator();
             String date;
 
             //それぞれの変数に読み込んだ結果をセットします。
@@ -85,113 +87,77 @@ public class Omikujiprogram {
             while ((date = br3.readLine()) != null) {
                 String[] values2 = date.split(",");
                 if (values2[0].equals(birthday) && values2[1].equals(todayString)) {
-                    //同じ結果を出します。
-                    unsei.setUnsei(values2[2]); //Unsei unsei = null; <<때문에 NullPointerException 남 ..........
+                    unsei = selectUnsei(values2[2]);
                     unsei.setUnsei();
                     unsei.setNegaigoto(values2[3]);
                     unsei.setAkinai(values2[4]);
                     unsei.setGakumon(values2[5]);
-                    //コンソールに結果を出力します。
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("今日の運勢は");
-                    sb.append(unsei.unsei);
-                    sb.append("です");
-                    sb.append("\n願い事：");
-                    sb.append(unsei.negaigoto);
-                    sb.append("\n商い：");
-                    sb.append(unsei.akinai);
-                    sb.append("\n学問：");
-                    sb.append(unsei.gakumon);
-                    sb.append(ls);
-                    System.out.println(sb.toString());
-                    //csvファイルに結果を出力します。
-                    String sb2 = birthday +
-                            "," +
-                            todayString +
-                            "," +
-                            unsei.unsei +
-                            "," +
-                            unsei.negaigoto +
-                            "," +
-                            unsei.akinai +
-                            "," +
-                            unsei.gakumon +
-                            ls;
-
-                    fw.write(sb2);
-                    fw.flush();
-
-                    fw.close();
-                    return;
                 }
             }
-        } catch (IOException e) {
+            br3.close();
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
+        // 運勢オブジェクトがnullであれば結果が無いという事、ランダムに１枚引く必要あり
+        if (unsei == null) {
 
-        //4. omkj.csv 파일을 읽어오기, unsei가 null일 경우 넣어줌
-        // OMKJ.CSV ファイルを読み込む、UNSEIに入れる(UNSEIがNULLではない場合)
+            //3. omkj.csv 파일을 읽어서 unsei가 null일 경우 넣어줌
+            // OMKJ.CSV ファイルを読み込んでUNSEIに入れる(UNSEIがNULLではない場合)
 
-        //Unseiのオブジェクト:unseiが空いている場合
-        //unseiが空いている場合、結果をセットするためにif文を使います。
-        String path = "omikuji/omkj.csv";
-        String line;
-        BufferedReader br = new BufferedReader(new FileReader(path));
+            //Unseiのオブジェクト:unseiが空いている場合
+            //unseiが空いている場合、結果をセットするためにif文を使います。
+            String path = "omikuji/omkj.csv";
+            String line;
+            BufferedReader br = new BufferedReader(new FileReader(path));
 
-        //omkj.csvから値を読み込みます。omkj.csvファイルは大吉、中吉、小吉、…、凶の結果が入っています。
-        //この値を入れるString [] valuesを作ります。
-        //readLineを使って１行ずつ読みます。値の区分は”,”で切って読み込みます。
+            //omkj.csvから値を読み込みます。omkj.csvファイルは大吉、中吉、小吉、…、凶の結果が入っています。
+            //この値を入れるString [] valuesを作ります。
+            //readLineを使って１行ずつ読みます。値の区分は”,”で切って読み込みます。
 
-        try {
-            String[] values; //1줄씩 받아서 저장할 어레이
-            br.readLine();
-            //1줄씩 csv파일 읽어오기 １行ずつcsvファイルから読み込む
-            while ((line = br.readLine()) != null) {
-                values = line.split(",");
+            try {
+                String[] values; //1줄씩 받아서 저장할 어레이
+                // 1行目を読み飛ばす
+                br.readLine();
+                //1줄씩 csv파일 읽어오기 １行ずつcsvファイルから読み込む
+                while ((line = br.readLine()) != null) {
+                    values = line.split(",");
 
-                //子クラス型の変数に入れる
-                //switch문으로 운세 지정해서 넣어주기
-                //Switch文を使って、作った抽象クラスを受け継いだ子クラス(Daikichi.java, Cyuukichi.java, …, Kyou.java)に
-                //それぞれの値をnewします。
-                switch (values[0]) {
-                    case "大吉" -> unsei = new Daikichi();
-                    case "中吉" -> unsei = new Cyuukichi();
-                    case "小吉" -> unsei = new Syoukichi();
-                    case "吉" -> unsei = new Kichi();
-                    case "末吉" -> unsei = new Sueyosi();
-                    case "凶" -> unsei = new Kyou();
-                    default -> {
-                    }
+                    //子クラス型の変数に入れる
+                    //switch문으로 운세 지정해서 넣어주기
+                    //Switch文を使って、作った抽象クラスを受け継いだ子クラス(Daikichi.java, Cyuukichi.java, …, Kyou.java)に
+                    //それぞれの値をnewします。
+                    unsei = selectUnsei(values[0]);
+
+                    //Unseiのmethodにそれぞれの値をセットします。
+                    //それぞれの値をセットして、作ったArrayListである omikujiに入れます。
+                    //最後、BufferedReaderを閉めて、例外処理をします。
+                    unsei.setUnsei();
+                    unsei.setNegaigoto(values[1]);
+                    unsei.setAkinai(values[2]);
+                    unsei.setGakumon(values[3]);
+                    omikuji.add(unsei);
                 }
-
-                //Unseiのmethodにそれぞれの値をセットします。
-                //それぞれの値をセットして、作ったArrayListである omikujiに入れます。
-                //最後、BufferedReaderを閉めて、例外処理をします。
-                unsei.setUnsei();
-                unsei.setNegaigoto(values[1]);
-                unsei.setAkinai(values[2]);
-                unsei.setGakumon(values[3]);
-                omikuji.add(unsei);
+                br.close();
             }
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //5. 랜덤으로 출력하기 ランダム結果を出力する
+            int rannum = new Random().nextInt(omikuji.size());
+            unsei = omikuji.get(rannum);
         }
-
-
-        //5. 랜덤으로 출력하기 ランダム結果を出力する
-        int rannum = new Random().nextInt(omikuji.size());
-        unsei = omikuji.get(rannum);
+        // 結果出力
         System.out.println(unsei.disp());
 
-
         //6. result.csv 파일에 입력 RESULT.CSVファイルに結果を書く
-
         //StringBuilderを使ってそれぞれの値を保存します。
         //fw.write(sb.toString())で保存した値を書いて、fw.flush()で全部放出します。
         //fw.close()でFileWriterを終了します。
-        try{
+
+        try {
             FileWriter fw = new FileWriter(result, true);
             String ls = System.lineSeparator();
             StringBuilder sb = new StringBuilder();
@@ -199,22 +165,20 @@ public class Omikujiprogram {
             sb.append(",");
             sb.append(todayString);
             sb.append(",");
-            sb.append(unsei.unsei);
+            sb.append(unsei.getUnsei());
             sb.append(",");
-            sb.append(unsei.negaigoto);
+            sb.append(unsei.getNegaigoto());
             sb.append(",");
-            sb.append(unsei.akinai);
+            sb.append(unsei.getAkinai());
             sb.append(",");
-            sb.append(unsei.gakumon);
+            sb.append(unsei.getGakumon());
             sb.append(ls);
 
             fw.write(sb.toString());
             fw.flush();
-
             fw.close();
-
         }
-        catch (Exception e){
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -223,5 +187,33 @@ public class Omikujiprogram {
     public static void main(String[] args) throws Exception {
         Omikujiprogram omikujiprogram = new Omikujiprogram();
         omikujiprogram.omikujii();
+    }
+
+    // 運勢からオブジェクトを判断してインスタンスを生成して返す
+    public static Unsei selectUnsei(String unseistr) {
+        Unsei unsei = null;
+        switch (unseistr) {
+            case "大吉":
+                unsei = new Daikichi();
+                break;
+            case "中吉":
+                unsei = new Cyuukichi();
+                break;
+            case "小吉":
+                unsei = new Syoukichi();
+                break;
+            case "吉":
+                unsei = new Kichi();
+                break;
+            case "末吉":
+                unsei = new Sueyosi();
+                break;
+            case "凶":
+                unsei = new Kyou();
+                break;
+            default:
+                break;
+        }
+        return unsei;
     }
 }
